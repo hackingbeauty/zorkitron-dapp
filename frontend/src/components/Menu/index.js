64122232c79469c3dd66442031c51edcd2bea400
Mark Muskardin <markmuskardin@gmail.com>
2025-03-14 17:29:56 -0700

    Added token images

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -6,6 +6,28 @@ import {
     Fade
 } from '@material-ui/core'
 
+import daiToken from 'assets/icons/tokens/dai.png'
+import usdtToken from 'assets/icons/tokens/usdt.png'
+import ethToken from 'assets/icons/tokens/eth.png'
+import rethToken from 'assets/icons/tokens/reth.png'
+import usdcToken from 'assets/icons/tokens/usdc.png'
+import wstethToken from 'assets/icons/tokens/wsteth-arbitrum.png'
+import usdsToken from 'assets/icons/tokens/usds.png'
+import uniToken from 'assets/icons/tokens/uni.png'
+import udsceToken from 'assets/icons/tokens/usdc.e.png'
+import wbtcToken from 'assets/icons/tokens/wbtc.png'
+import wethToken from 'assets/icons/tokens/weth.png'
+import lidoToken from 'assets/icons/tokens/lido.png'
+import linkToken from 'assets/icons/tokens/link.png'
+import mkrToken from 'assets/icons/tokens/mkr.png'
+import compToken from 'assets/icons/tokens/comp.png'
+import arbToken from 'assets/icons/tokens/arb.png'
+import weethToken from 'assets/icons/tokens/weeth.png'
+import usdeToken from 'assets/icons/tokens/usde.png'
+import cbbtcToken from 'assets/icons/tokens/cbbtc.png'
+import { styles } from './styles.scss'
+
+const ITEM_HEIGHT = 18;
 export default function Menu() {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
@@ -17,46 +39,93 @@ export default function Menu() {
     };
 
     return (
-        <div>
-        <Button
-            id="fade-button"
-            aria-controls={open ? 'fade-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-        >
-            Dashboard
-        </Button>
-        <MaterialUIMenu
-            id="fade-menu"
-            MenuListProps={{
-            'aria-labelledby': 'fade-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            TransitionComponent={Fade}
-        >
-            <MenuItem onClick={handleClose}>DAI</MenuItem>
-            <MenuItem onClick={handleClose}>USDT</MenuItem>
-            <MenuItem onClick={handleClose}>ETH</MenuItem>
-            <MenuItem onClick={handleClose}>RETH</MenuItem>
-            <MenuItem onClick={handleClose}>USDC</MenuItem>
-            <MenuItem onClick={handleClose}>WSTETH</MenuItem>
-            <MenuItem onClick={handleClose}>USDS</MenuItem>
-            <MenuItem onClick={handleClose}>UNI</MenuItem>
-            <MenuItem onClick={handleClose}>UDSC.E</MenuItem>
-            <MenuItem onClick={handleClose}>WBTC</MenuItem>
-            <MenuItem onClick={handleClose}>WETH</MenuItem>
-            <MenuItem onClick={handleClose}>LDO</MenuItem>
-            <MenuItem onClick={handleClose}>LINK</MenuItem>
-            <MenuItem onClick={handleClose}>MKR</MenuItem>
-            <MenuItem onClick={handleClose}>COMP</MenuItem>
-            <MenuItem onClick={handleClose}>ARB</MenuItem>
-            <MenuItem onClick={handleClose}>WEETH</MenuItem>
-            <MenuItem onClick={handleClose}>USDE</MenuItem>
-            <MenuItem onClick={handleClose}>CBBTC</MenuItem>
-        </MaterialUIMenu>
+        <div className={styles}>
+            <Button
+                id="fade-button"
+                aria-controls={open ? 'fade-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                Dashboard
+            </Button>
+            <MaterialUIMenu
+                id="long-menu"
+                className={styles}
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                // TransitionComponent={Fade}
+                slotProps={{
+                    paper: {
+                        style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: '20ch',
+                        },
+                    },
+                }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={daiToken} />DAI
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={usdtToken} />USDT
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={ethToken} />ETH
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={rethToken} />RETH
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={usdcToken} />USDC
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={wstethToken} />WSTETH
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={usdsToken} />USDS
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={uniToken} />UNI
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={udsceToken} />UDSC.E
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={wbtcToken} />WBTC
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={wethToken} />WETH
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={lidoToken} />LDO
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={linkToken} />LINK
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={mkrToken} />MKR
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={compToken} />COMP
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={arbToken} />ARB
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={weethToken} />WEETH
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={usdeToken} />USDE
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <img width="20" src={cbbtcToken} />CBBTC
+                </MenuItem>
+            </MaterialUIMenu>
         </div>
     );
 }
